719f7713d1fc675f64fbe0fd17e2b20e103234d0
saurabhm719 <53507435+TheMythicCoder@users.noreply.github.com>
2026-05-23 22:27:05 +0530

    cgs

--- test6.cpp
+++ test6.cpp
@@ -1,23 +1,50 @@
-#include &lt;iostream&gt;
-#include &lt;cstdlib&gt;
-#include &lt;ctime&gt;
-
+#include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    srand(time(0));
-    cout &lt;&lt; "Random numbers:" &lt;&lt; endl;
-    for(int i = 0; i &lt; 5; i++) {
-        cout &lt;&lt; rand() % 100 &lt;&lt; endl;
-    }
-    cout &lt;&lt; "Another set:" &lt;&lt; endl;
-    for(int i = 0; i &lt; 5; i++) {
-        cout &lt;&lt; rand() % 100 &lt;&lt; endl;
-    }
-    cout &lt;&lt; "More random:" &lt;&lt; endl;
-    for(int i = 0; i &lt; 5; i++) {
-        cout &lt;&lt; rand() % 100 &lt;&lt; endl;
-    }
-    cout &lt;&lt; "End of random code." &lt;&lt; endl;
-    return 0;
+	string s1 = "Hello";
+	string s2 = "World";
+	cout << "s1: " << s1 << endl;
+	cout << "s2: " << s2 << endl;
+
+	// Concatenation
+	string s3 = s1 + " " + s2;
+	cout << "Concatenated: " << s3 << endl;
+
+	// Length
+	cout << "Length of s3: " << s3.length() << endl;
+
+	// Substring
+	string sub = s3.substr(0, 5);
+	cout << "Substring: " << sub << endl;
+
+	// Find
+	size_t pos = s3.find("World");
+	if (pos != string::npos)
+		cout << "'World' found at: " << pos << endl;
+
+	// Replace
+	s3.replace(6, 5, "C++");
+	cout << "After replace: " << s3 << endl;
+
+	// Insert
+	s3.insert(6, "amazing ");
+	cout << "After insert: " << s3 << endl;
+
+	// Erase
+	s3.erase(6, 8);
+	cout << "After erase: " << s3 << endl;
+
+	// Reverse
+	reverse(s3.begin(), s3.end());
+	cout << "Reversed: " << s3 << endl;
+
+	// Iterate
+	cout << "Characters: ";
+	for (char c : s3) cout << c << ' ';
+	cout << endl;
+
+	return 0;
 }
